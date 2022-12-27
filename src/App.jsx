8c7f5f9489894e238760f8125d27887dd99fc136
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-12-27 19:37:37 +0100

    ajout des différents composants de la to do

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 import './App.css'
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className='app-wrapper'>
+      <h1>Tasks</h1>
+      <Form />
+
+      <Todo />
+    </div>
+  );
+}
+
+const Form = () => {
+  return <div></div>
+}
+
+const Todo = () => {
+  return (
+    <div>
+      <Checkbox />
+    </div>
+  )
+}
+
+const Checkbox = () => {
+  return <div></div>
 }
