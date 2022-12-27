bd04c6359261a153aaab4672118ba6ce8a0554f2
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2022-12-27 20:29:22 +0100

    ajout de l'événement quand la case est choqué

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 
 export default function App() {
@@ -33,9 +34,25 @@ const Todo = () => {
     <div>
       <Checkbox />
     </div>
-  )
-}
+  );
+};
 
 const Checkbox = () => {
-  return <div></div>
-}
+  const [checked, setChecked] = useState(false);
+
+  const onChange = (event) => {
+    setChecked(event.target.checked);
+  }
+
+  return (
+    <div className='checkbox'>
+    <input type="checkbox" checked={checked} onChange={onChange} />
+    {checked && (
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M5 12l5 5l10 -10"></path>
+      </svg>
+    )}
+  </div>
+  );
+};
